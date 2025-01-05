7030ded0d88d4d3c62511116b7f9eaaae02cc007
Beomjun Kim <86099741+i3ullbum@users.noreply.github.com>
2025-01-05 22:43:25 +0900

    Update vit_pretrain_imagenet1k.sh
    modified imagenet1k training hyperparameters

--- vit_pretrain_imagenet1k.sh
+++ vit_pretrain_imagenet1k.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 models=("vit_tiny_patch16_224" "vit_small_patch16_224" "vit_base_patch16_224")
-lrs=("0.0001" "0.0003" "0.001" "0.003" "0.01")
+lrs=("1e-4" "3e-4" "1e-3" "3e-3" "1e-2")
 
 for model in "${models[@]}"; do
   for lr in "${lrs[@]}"; do
-    CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 ./distributed_train.sh 8 \
+    ./distributed_train.sh 8 \
       --model $model \
       --dataset imagenet \
       --data-dir ~/data/imagenet1k \
-      --mean 0.485, 0.456, 0.406 --std 0.229, 0.224, 0.225 \
+      --mean 0.485 0.456 0.406 --std 0.229 0.224 0.225 \
       --num-classes 1000 \
       --img-size 224 \
-      --batch-size 1024 \
+      --batch-size 4096 \
       --epochs 300 \
       --lr $lr \
-      --warmup-epochs 10 \
-      --weight-decay 0.1 \
-      --mixup 0.8 \
-      --cutmix 1.0 \
+      --warmup-epochs 5 \
+      --weight-decay 0.3 \
       --smoothing 0.1 \
       --sched cosine \
       --opt adamw \

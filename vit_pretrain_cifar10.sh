85edc93278fae4dbedfdeadd5dfa3dc9b3ba2050
Beomjun Kim <86099741+i3ullbum@users.noreply.github.com>
2025-01-06 16:51:12 +0900

    Update vit_pretrain_cifar10.sh

--- vit_pretrain_cifar10.sh
+++ vit_pretrain_cifar10.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 models=("vit_tiny_patch16_224" "vit_small_patch16_224" "vit_base_patch16_224")
-lrs=("0.0001" "0.0003" "0.001" "0.003" "0.01")
+lrs=("1e-4" "3e-4" "1e-3" "3e-3" "1e-2")
 
 for model in "${models[@]}"; do
   for lr in "${lrs[@]}"; do
-    CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 ./distributed_train.sh 8 \
+    ./distributed_train.sh 8 \
       --model $model \
       --dataset torch/cifar10 \
       --data-dir ~/data/cifar10 \
@@ -13,20 +13,18 @@ for model in "${models[@]}"; do
       --mean 0.4914 0.4822 0.4465 --std 0.2023 0.1994 0.2010 \
       --num-classes 10 \
       --img-size 224 \
-      --batch-size 256 \
+      --batch-size 1024 \
       --epochs 300 \
       --lr $lr \
-      --warmup-epochs 10 \
-      --weight-decay 0.1 \
-      --mixup 0.8 \
-      --cutmix 1.0 \
+      --warmup-epochs 5 \
+      --weight-decay 0.3 \
       --smoothing 0.1 \
       --sched cosine \
       --opt adamw \
       --drop-path 0.1 \
       --seed 1234 \
       --log-interval 1 \
-      --output ./output/${model}_lr${lr} \
+      --output ./output/cifar10/${model}_lr${lr} \
       --log-wandb \
       --experiment CIFAR10
   done
